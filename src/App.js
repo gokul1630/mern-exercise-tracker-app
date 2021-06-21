a9c1ff9362a76b59ffd59917cf291b57b463a498
gokul1630 <gokulgp163034@gmail.com>
2021-06-21 14:54:10 +0530

    use HashRouter instead of Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { HashRouter, Route } from 'react-router-dom';
 import CreateExercise from './components/CreateExercise';
 import CreateUser from './components/CreateUser';
 import EditExercise from './components/EditExercise';
 import ExerciseList from './components/ExerciseList';
 import Navbar from './components/Navbar';
 import './styles.css';
+
 class App extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <HashRouter basename='/'>
         <div>
           <Navbar />
           <Route exact path='/' component={ExerciseList}></Route>
@@ -18,7 +18,7 @@ class App extends Component {
           <Route path='/createUser' component={CreateUser}></Route>
           <Route path='/editExercise/:id' component={EditExercise}></Route>
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
