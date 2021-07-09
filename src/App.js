f70c1ea09a685f3a3e3632a93008f3346dae9680
gokul1630 <gokulgp163034@gmail.com>
2021-07-09 12:09:10 +0530

    Router: use Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import CreateExercise from './components/CreateExercise';
 import CreateUser from './components/CreateUser';
 import EditExercise from './components/EditExercise';
@@ -11,13 +11,13 @@ class App extends Component {
   render() {
     return (
       <HashRouter basename='/'>
-        <div>
-          <Navbar />
+        <Navbar />
+        <Switch>
           <Route exact path='/' component={ExerciseList}></Route>
           <Route path='/createExercise' component={CreateExercise}></Route>
           <Route path='/createUser' component={CreateUser}></Route>
           <Route path='/editExercise/:id' component={EditExercise}></Route>
-        </div>
+        </Switch>
       </HashRouter>
     );
   }
